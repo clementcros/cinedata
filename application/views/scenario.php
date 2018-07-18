5fc817f2a61635a242cfb8d82893bcf88c0a8115
Maxime <monroy.maxime@hotmail.fr>
2018-07-18 17:06:57 +0200

    Fix scenario displaying title

--- application/views/scenario.php
+++ application/views/scenario.php
@@ -5,21 +5,22 @@
 
          <div class="card col-md-6">
              <div class="col-lg-6">
-                 <img class="card-img-top" src="<?php
-                 if ($article_item['image'] != null){
-
-                     echo base_url() . '/assets/uploads/files/' . $article_item['image'];
-                 }
-                 else {
-                     echo base_url() . '/images/clap-1.png';
-                 }
-
+                 <img class="card-img-top" src="
+                 <?php
+                     if ($article_item['image'] != null){
+                         echo base_url() . '/assets/uploads/files/' . $article_item['image'];
+                     }
+                     else {
+                         echo base_url() . '/images/clap-1.png';
+                     }
                  ?>" alt="Card image cap">
              </div>
              <div class="card" style="width: 50rem;">
                  <div class="card-body text-center">
-                     <h2 class="card-title"><a><?php print_r( $article_item['nom']);?></a></h2>
-                     <p class="card-text"><?php print_r( $article_item['desciption']);?></p>
+                     <h2 class="card-title"><a><?php print_r($article_item['nom']);?></a></h2>
+                     <p class="card-text">
+                         <?php print_r($article_item['desciption']);?>
+                     </p>
                      <a href="<?php echo base_url().'assets/uploads/files/'.$article_item['file_url'];?>" class="btn btn-primary";">Télécharger </a>
                  </div>
              </div>
@@ -35,6 +36,7 @@
         height:250px;
     }
     .card.col-md-6 {
+        word-wrap: break-word;
         margin-top: 5rem;
         margin-bottom: 5rem;
     }
