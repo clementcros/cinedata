b34faa8e0c46aa9d0cd271c9a0eee5a2ff54c537
Maxime <monroy.maxime@hotmail.fr>
2018-07-19 16:26:20 +0200

    CSS contact form

--- application/views/contact_valid.php
+++ application/views/contact_valid.php
@@ -2,8 +2,8 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-12 col-lg-12">
-                <h2 class="h2">Votre message à bien été envoyé</h2>
-                <h2 class="h3">Vous recevrez une réponse sous peu</h3>
+                <h2 class="h2">Votre message a bien été envoyé</h2>
+                <h2 class="h3">Nous vous répondrons rapidement</h3>
             </div>
         </div>
     </div>
